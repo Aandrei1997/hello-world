43b41aa7204afc0be35e39761ac03aa49027776f
Aandrei1997 <32851158+Aandrei1997@users.noreply.github.com>
2017-11-05 22:49:24 +0200

    Update T1_EX_E3_functie_Calinescu_Andrei_Mihai_grupa422D.m

--- T1_EX_E3_functie_Calinescu_Andrei_Mihai_grupa422D.m
+++ T1_EX_E3_functie_Calinescu_Andrei_Mihai_grupa422D.m
@@ -1,8 +1,8 @@
 function [media, vector, matrice] = functie(z)
 
 media=mean(real(z))
-vector=z.^2
-matrice=z*(z.')
+vector=z.^2   %se ridica la patrat toate elementele vectorului
+matrice=z*(z.')   %se inmulteste matricea z cu transpusa ei
 
 
 end
